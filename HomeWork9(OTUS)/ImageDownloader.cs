c5f8a4270ea42913de81eafd7c82e26668f3b5aa
Владимир <@NIKONOV>
2022-11-13 20:26:47 +0500

    Добавьте файлы проекта.

---
+++ HomeWork9(OTUS)/ImageDownloader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9_OTUS
+{
+    internal class ImageDownloader
+    {
+        public event Action<string> ImageStarted;
+        public event Action<string> ImageCompleted;
+        public void Download()
+        {            
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            string fileName = "bigimage4.jpg";
+
+            // Качаем картинку в текущую директорию
+            var myWebClient = new WebClient();
+            ImageStarted?.Invoke("Скачивание файла началось");
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+            myWebClient.DownloadFile(remoteUri, fileName);
+            ImageCompleted?.Invoke("Скачивание файла закончилось");
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+
+        public async Task DownloadAsync()
+        {
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            string fileName = "bigimage5.jpg";
+            var myWebClient = new WebClient();            
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);            
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }       
+            
+    }
+}
---
+++ HomeWork9(OTUS)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace HomeWork9_OTUS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var downloader= new ImageDownloader();
+            downloader.ImageStarted += ShowStatus;
+            downloader.ImageCompleted += ShowStatus;
+
+            downloader.Download();
+
+        }
+
+        public static void ShowStatus(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
