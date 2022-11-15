4ed55947cb60ceae750d2c7d0df0c96fed37d7ad
MukIlnur <24mi@bk.ru>
2022-11-15 11:34:51 +0500

    Добавил вывод результата скачивания

--- HomeWork9(OTUS)/ImageDownloader.cs
+++ HomeWork9(OTUS)/ImageDownloader.cs
@@ -11,32 +11,30 @@ namespace HomeWork9_OTUS
     {
         public event Action<string> ImageStarted;
         public event Action<string> ImageCompleted;
+        WebClient myWebClient = new WebClient();
+        string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+        string fileName;
+        
         public void Download()
-        {            
-            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
-            string fileName = "bigimage4.jpg";
-
-            // Качаем картинку в текущую директорию
-            var myWebClient = new WebClient();
+        {
+            fileName = "bigimage.jpg";            
+            
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n", fileName, remoteUri);
             ImageStarted?.Invoke("Скачивание файла началось");
-            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
             myWebClient.DownloadFile(remoteUri, fileName);
-            ImageCompleted?.Invoke("Скачивание файла закончилось");
-            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
-            Console.WriteLine("Нажмите любую клавишу для выхода");
-            Console.ReadKey();
+            ImageCompleted?.Invoke("Скачивание файла закончилось \n");
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"\n", fileName, remoteUri);            
         }
 
         public async Task DownloadAsync()
-        {
-            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
-            string fileName = "bigimage5.jpg";
-            var myWebClient = new WebClient();            
-            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
-            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);            
-            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
-            Console.WriteLine("Нажмите любую клавишу для выхода");
-            Console.ReadKey();
+        {            
+            fileName = "bigimage2.jpg";            
+            
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n", fileName, remoteUri);
+            ImageStarted?.Invoke("Скачивание файла началось");
+            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            ImageCompleted?.Invoke("Скачивание файла закончилось \n");
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);           
         }       
             
     }
--- HomeWork9(OTUS)/Program.cs
+++ HomeWork9(OTUS)/Program.cs
@@ -9,9 +9,24 @@ namespace HomeWork9_OTUS
             var downloader= new ImageDownloader();
             downloader.ImageStarted += ShowStatus;
             downloader.ImageCompleted += ShowStatus;
-
             downloader.Download();
 
+            Console.WriteLine("Нажмите любую клавишу для выхода продолжения");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Task task = downloader.DownloadAsync();            
+            Console.WriteLine("Нажмите клавишу 'A' или 'a' для выхода или любую другую клавишу для проверки статуса скачивания");            
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if (key.KeyChar.ToString().ToLower() != "a")
+            {
+                if (task.IsCompleted)
+                    Console.WriteLine("Асихронное скачивание выполнено успешно!"); 
+                else
+                    Console.WriteLine("Асихронное скачивание не выполнено!");
+            }
         }
 
         public static void ShowStatus(string text)
